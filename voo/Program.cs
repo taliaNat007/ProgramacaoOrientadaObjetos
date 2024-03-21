c6c38508c2fceda9b86da1ad4d7091056935b4e0
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-03-20 22:20:25 -0400

    Add files via upload

---
+++ voo/Program.cs
@@ -0,0 +1,13 @@
+﻿DateOnly data = DateOnly.FromDateTime(DateTime.Now);
+Voo voo = new Voo(100, data, 1);
+
+/*for (int i = 0; i < voo.qtdVagas; i++)
+{
+    Console.WriteLine(voo.vagas[i]);
+}*/
+
+
+Console.WriteLine(voo.Verificar(8));
+voo.Ocupa(8);
+Console.WriteLine(voo.Verificar(8));
+voo.PoltronasLivre(100);
---
+++ voo/Voo.cs
@@ -0,0 +1,70 @@
+﻿public class Voo
+{
+    public DateOnly datavoo;
+    public TimeOnly horavoo;
+    public int n_voo;
+
+
+    public bool[] cadeiras;
+    public int qtdVagas;
+
+    public Voo(int qtdVagas, DateOnly datVoo, int nVoo)
+    {
+        // comando (this.) --> utilizado para referenciar que a variável que utilizarei é de fora
+        // do construtor
+        this.datavoo = datVoo;
+        n_voo = nVoo;
+
+        cadeiras = new bool[qtdVagas];
+        this.qtdVagas = qtdVagas;
+    }
+
+    public int ProxPoltrona()
+    {
+        for (int i = 0; i < cadeiras.Length; i++)
+        {
+            if (cadeiras[i] == false)
+            {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    public bool Verificar(int poltrona)
+    {
+        if (cadeiras[poltrona - 1] == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public bool Ocupa(int poltrona)
+    {
+        if (this.Verificar(poltrona) == false)
+        {
+            cadeiras[poltrona - 1] = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int PoltronasLivre(int poltrona = 0)
+    {
+        for (int i = 0; i < cadeiras.Length; i++)
+        {
+            if (cadeiras[i] == false)
+            {
+                poltrona = +1;
+            }
+        }
+        return poltrona;
+    }
+}
