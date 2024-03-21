6d2b2387f07fa75a88ee9a73c5df4e7c97d6710b
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-03-20 22:16:50 -0400

    Add files via upload

---
+++ Atividade_Encapsulamento/Produto.cs
@@ -0,0 +1,72 @@
+﻿    public class Produto
+    {
+        // questão 1: criação da classe
+        public string cod_prod;
+        public string nome;
+        private string unidade;
+        private double preco;
+        public float quantidade;
+
+        public void SetPreco(double preco)
+        {
+            if (preco > 0)
+            {
+                this.preco = preco;
+            }
+            else 
+            {
+                Console.WriteLine(" O preço do produto deve ser maior R$ 0,00");
+            }
+        }// FIM DO SET VERIFICAR PRECO
+
+        public double GetPreco()
+        { 
+            return preco;
+        }// FIM DO GET VERIFICAR PREÇO
+
+        public void SetTipoUnidade(string unidade)
+        {
+            if (unidade == "LT")
+            {
+                this.unidade = unidade;
+            }
+            else 
+            {
+                if (unidade == "CX")
+                {
+                    this.unidade = unidade;
+                }
+                else
+                {
+                    if (unidade == "KG")
+                    {
+                        this.unidade = unidade;
+                    }
+                        else
+                    {
+                        if (unidade == "UN")
+                        {
+                            this.unidade = unidade;
+                        }
+                        else
+                        {
+                            if (unidade == "MT")
+                            {
+                                this.unidade = unidade;
+                            }
+                            else
+                            {
+                                Console.WriteLine(" Opção Inexistente!");
+                            }
+                        }
+                    }
+                }
+            }
+        }// FIM DO SET VERIFICAR TIPO DE UNIDADE
+
+        public string GetTipoUnidade()
+        {
+            return unidade;
+        }// FIM DO GET VERIFICAR TIPO DE UNIDADE
+}
+
---
+++ Atividade_Encapsulamento/Program.cs
@@ -0,0 +1,79 @@
+﻿List<Produto> listaproduto = new List<Produto>();
+
+while (true)
+{
+    Console.WriteLine(@"    Qual opção deseja realizar
+                         Digite 1 | Cadastrar
+                         Digite 2 | Listar
+                         Digite 3 | Excluir ");
+    int opc = Convert.ToInt32(Console.ReadLine());
+    
+    Produto produto1 = new Produto();
+
+    if (opc == 1 )
+    {
+        // Cadastrando Produto
+        Console.WriteLine("Insira o código do produto: ");
+        produto1.cod_prod = Console.ReadLine();
+
+        Console.WriteLine("Insira o nome do produto: ");
+        produto1.nome = Console.ReadLine();
+        
+        Console.WriteLine(@" Digite o tipo da unidade do produto
+                                    LT | Litro
+                                    CX | Caixa
+                                    KG | Kilo grama
+                                    UN | Unidade
+                                    MT | Metros: ");
+        string unidade = Console.ReadLine();
+        produto1.SetTipoUnidade(unidade);
+        
+        Console.WriteLine("Insira o preço do produto: ");
+        double preco = Convert.ToDouble(Console.ReadLine());
+        produto1.SetPreco(preco);
+
+        Console.WriteLine(" Insira a quantidade do produto: ");
+        produto1.quantidade = Convert.ToInt32(Console.ReadLine());
+
+        listaproduto.Add(produto1);
+    }
+    else if (opc == 2)
+    {
+        //Listando Produto
+            foreach (Produto produto in listaproduto)
+            {
+                Console.WriteLine(" Código do produto: " + produto.cod_prod);
+                Console.WriteLine(" Nome do produto: " + produto.nome);
+                Console.WriteLine(" Unidade do produto: " + produto.GetTipoUnidade());
+                Console.WriteLine(" Preco do produto R$ " + produto.GetPreco() + ",00");
+                Console.WriteLine(" Quantidade do produto: " + produto.quantidade);
+            }
+    }
+    else if (opc == 3)
+    {
+        Console.WriteLine(" Digite o Código do produto que deseja EXCLUIR");
+        string codigo = Console.ReadLine();
+
+        Produto prodExcluir = listaproduto.FirstOrDefault(p => p.cod_prod == codigo);
+        listaproduto.Remove(prodExcluir);
+    }
+}
+
+
+/*
+Console.WriteLine("Insira o preço do produto");
+double preco = Convert.ToDouble(Console.ReadLine());
+produto1.SetPreco(preco);
+
+Console.WriteLine(@" Digite o tipo da unidade do produto
+                                    LT | Litro
+                                    CX | Caixa
+                                    KG | Kilo grama
+                                    UN | Unidade
+                                    MT | Metros");
+string unidade = Console.ReadLine(); 
+produto1.SetTipoUnidade(unidade);
+
+Console.WriteLine($"O preço do produto é R$ {produto1.GetPreco()},00: ");
+Console.WriteLine("A unidade do produto é: " + produto1.GetTipoUnidade());
+*/
