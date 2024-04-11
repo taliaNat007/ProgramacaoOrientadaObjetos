a43fe41abd31691b075f5d2415292e432dbaac37
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-04-10 22:00:15 -0400

    Add files via upload

---
+++ revisao/Matematica.cs
@@ -0,0 +1,18 @@
+﻿public class Matematica
+{
+    public double n1;
+    public double n2;
+    public double soma;
+    public double multiplicacao;
+    public double Soma()
+    {
+        soma = n1 + n2;
+        return soma;
+    }
+
+    public double Multiplicar() 
+    {
+        multiplicacao = n1 * n2;
+        return multiplicacao; 
+    }
+}
---
+++ revisao/Program.cs
@@ -0,0 +1,20 @@
+﻿Matematica soma = new Matematica();
+Matematica multiplicacao = new Matematica();
+
+Console.WriteLine(" Digite um número para somar: ");
+soma.n1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" Digite um número para somar: ");
+soma.n2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" Digite um número para multiplicar: ");
+multiplicacao.n1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(" Digite um multiplicar: ");
+multiplicacao.n2 = double.Parse(Console.ReadLine());
+
+double somafinal = soma.Soma();
+double multiplicacaofinal = multiplicacao.Multiplicar();
+
+Console.WriteLine($" A soma dos números {soma.n1} e {soma.n2} é: " + soma.Soma()) ;
+Console.WriteLine($" A multiplicação dos números {multiplicacao.n1} e {soma.n2} é: " + multiplicacao.Multiplicar()) ;
