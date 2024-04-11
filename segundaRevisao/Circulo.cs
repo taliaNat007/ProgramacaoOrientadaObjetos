04f82877917b322868022f6032e857749520850b
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-04-10 22:19:20 -0400

    Add files via upload

---
+++ segundaRevisao/Circulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundaRevisao
+{
+    internal class Circulo
+    {
+        public double raio = 0;
+        public double area = 0;
+        public double Area()
+        {
+            area = 3.14 * (raio * raio);
+            return area;
+        }
+    }
+}
---
+++ segundaRevisao/Program.cs
@@ -0,0 +1,10 @@
+﻿using segundaRevisao;
+
+Circulo circulo1 = new Circulo();
+
+Console.WriteLine(" Digite o raio do seu circulo: ");
+circulo1.raio = double.Parse(Console.ReadLine());
+
+double area = circulo1.Area();
+
+Console.WriteLine(" A área do circulo é: " + circulo1.Area());
