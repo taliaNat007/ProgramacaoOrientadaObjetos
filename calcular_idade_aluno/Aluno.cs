67f51423e6378cc14529a4d4a6741093df6e87a0
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-07-17 21:14:14 -0400

    Revisão Pós Greve - Calcular idade do aluno
    Revisão pós greve.
    Calcular idade de um aluno de acordo com a data de nascimento.
    Revisando: Classe, Objeto, Construtores - cheio e vazio-, lógica de programação

---
+++ calcular_idade_aluno/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcular_idade_aluno
+{
+    internal class Aluno
+    {
+        public string nome;
+        public string matricula;
+        public DateTime data_nasc; 
+        public int idade_aluno;
+        public Aluno(DateTime data_nascimento)
+        {
+            this.data_nasc = data_nascimento;
+            DateTime data_atual = DateTime.Now;
+            int idade = data_atual.Year - this.data_nasc.Year;
+
+            if (data_atual.Month < this.data_nasc.Month)
+            {
+                idade -= 1;
+            }
+            else 
+            {
+                if (data_atual.Month == this.data_nasc.Month)
+                {
+                    if (data_atual.Day < this.data_nasc.Day)
+                    {
+                        idade = 1;
+                    }
+                }
+            }
+            this.idade_aluno = idade;
+         }
+    }
+}
---
+++ calcular_idade_aluno/Program.cs
@@ -0,0 +1,17 @@
+﻿// programa principal
+using calcular_idade_aluno;
+
+Aluno a1 = new Aluno( new DateTime(2003, 05, 28));
+
+a1.nome = "Natália";
+a1.matricula = "2022202060013";
+
+
+Console.WriteLine( $@"
+                        Dados do Aluno
+                        
+                        Nome: {a1.nome}
+                        Matricula: {a1.matricula}
+                        Data Nascimento: {a1.data_nasc}
+                        Idade: {a1.idade_aluno}"
+                   );
