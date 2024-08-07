90c4dedfa72363d41c3dee02adf787c8fb5423f6
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-08-07 19:55:52 -0400

    Add files via upload

---
+++ exercicio_excessoes/Program.cs
@@ -0,0 +1,32 @@
+﻿decimal num1, num2, div;
+
+Console.WriteLine(" Digite o primeiro número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Digite o segundo número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+
+// EXCESSÃO
+/*
+ 
+ NÃO TEM NADA HAVER COM A ESTRUTURA DE CONDIÇÃO IF. 
+ 
+ */
+try
+{
+    //Excessão principal
+    div = num1 / num2;
+
+    Console.WriteLine(" A divisão entre os números é: " + div);
+}
+catch (Exception excessao)
+{
+    // Excessão que PREVINE um BUG no sistema, caso a operação não seja válida ele avisa. No caso abaixo, não é possível dividir nenhum número por ZERO.
+    Console.WriteLine(" Não é permitido divisão por ZERO " + excessao.Message);
+}
+finally
+{
+    // Excesssão que ocorre independente das demais
+    Console.WriteLine(" Volte Sempre!!!");
+}
