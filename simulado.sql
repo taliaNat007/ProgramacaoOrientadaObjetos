ae3a9d5971612590653feef5c5ff742c43484cd4
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-08-14 22:25:55 -0400

    Add files via upload

---
+++ simulado.sql
@@ -0,0 +1,13 @@
+Create database simulado;
+use simulado;
+
+create table Alunos(
+Id_Aluno int primary key ,
+Nome varchar(100),
+Cpf varchar(15) ,
+Email varchar(150) ,
+Telefone varchar(13) ,
+Data_Nascimento date 
+);
+select * from Alunos;
+drop table Alunos;
