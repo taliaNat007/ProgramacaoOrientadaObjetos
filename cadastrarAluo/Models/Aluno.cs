d8c357f1c2dffaa9ed43fdd34e1526dde2c9e4a7
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-08-14 22:21:30 -0400

    Add files via upload

---
+++ cadastrarAluo/Models/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadastrarAluo.Models
+{
+    internal class Aluno
+    {
+        public int IdAluno { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateOnly DataNascimento { get; set; }
+
+           
+    }
+}
---
+++ cadastrarAluo/Program.cs
@@ -0,0 +1,47 @@
+﻿using cadastrarAluo;
+using cadastrarAluo.Dao;
+using cadastrarAluo.Models;
+
+try
+{
+	Conexao.Conectar();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("Erro" + ex.Message);
+}
+
+try
+{
+    // Cadastrando aluno 1
+    Aluno aluno = new Aluno();
+
+    aluno.IdAluno = 1;
+    aluno.Nome = "Natália";
+    aluno.Cpf = "058.102.632-24";
+    aluno.Email = "fernandesnatalia@gmail.com";
+    aluno.Telefone = "5569992075679";
+    aluno.DataNascimento = new DateOnly(2003, 05, 28);
+
+    AlunoDao alunoDao = new AlunoDao();
+    alunoDao.Insert(aluno);
+    
+    // Cadastrando aluno 2
+    Aluno aluno2 = new Aluno();
+
+    aluno2.IdAluno = 2;
+    aluno2.Nome = "Fátima";
+    aluno2.Cpf = "348.387.402-04";
+    aluno2.Email = "fat.fer457@gmail.com";
+    aluno2.Telefone = "5569992839065";
+    aluno2.DataNascimento = new DateOnly(2003, 05, 28);
+
+    AlunoDao alunoDao2 = new AlunoDao();
+    alunoDao2.Insert(aluno2);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine($"Erro! {ex.Message}");
+}
