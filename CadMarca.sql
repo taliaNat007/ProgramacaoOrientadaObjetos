ed18252a0dcf438fb0ced0db168d9417de6fe7aa
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-08-26 19:41:33 -0400

    Add files via upload

---
+++ CadMarca.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE CadMarca;
+use CadMarca;
+
+CREATE TABLE Marca(
+id_marca INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(255),
+observacao VARCHAR(255),
+localidade VARCHAR(255)
+);
+
