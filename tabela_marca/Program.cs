611c41a762d5425b3e4eaee50d0661b7fd122d1c
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2024-08-26 22:12:37 -0400

    Add files via upload

---
+++ tabela_marca/Program.cs
@@ -0,0 +1,28 @@
+﻿using tabela_marca;
+using tabela_marca.Models;
+using tabela_marca.DAO;
+
+try
+{
+    Conexao.Conectar();
+
+    Marca marcaupdate = new Marca();
+
+    Console.WriteLine(" Digite o ID: ");
+    marcaupdate.Id_marca = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine($" Digite um novo nome para a marca {marcaupdate.Nome}: ");
+    marcaupdate.Nome = Console.ReadLine();
+    Console.WriteLine($" Digite uma nova observação para a marca {marcaupdate.Nome}: ");
+    marcaupdate.Observacao = Console.ReadLine();
+    Console.WriteLine($" Digite a nova localidade da marca {marcaupdate.Nome}: ");
+    marcaupdate.Localidade = Console.ReadLine();
+    
+
+    MarcaDAO marcaUpDAO1 = new MarcaDAO();
+    marcaUpDAO1.Update(marcaupdate);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERRO! {ex.Message}");
+}
